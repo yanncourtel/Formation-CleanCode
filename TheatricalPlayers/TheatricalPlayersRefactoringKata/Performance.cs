543424f2beac9804a3367eb9d705f783298378dd
Hamza Tadhak <hamza.tadhak@cdbdx.biz>
2022-02-09 15:25:13 +0100

    refacto : add calculate amounts in performance

--- TheatricalPlayers/TheatricalPlayersRefactoringKata/Performance.cs
+++ TheatricalPlayers/TheatricalPlayersRefactoringKata/Performance.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TheatricalPlayersRefactoringKata
 {
     public class Performance
@@ -11,5 +13,39 @@ namespace TheatricalPlayersRefactoringKata
             this.Audience = audience;
         }
 
+        internal int GetTotalAmount()
+        {
+            var thisAmount = 0;
+            switch (Play.Type)
+            {
+                case "tragedy":
+                    thisAmount = 40000;
+                    if (Audience > 30)
+                    {
+                        thisAmount += 1000 * (Audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    thisAmount = 30000;
+                    if (Audience > 20)
+                    {
+                        thisAmount += 10000 + 500 * (Audience - 20);
+                    }
+                    thisAmount += 300 * Audience;
+                    break;
+                default:
+                    throw new Exception("unknown type: " + Play.Type);
+            }
+            return thisAmount;
+        }
+
+        internal int GetVolumeCredit()
+        {
+            // add volume credits
+            var volumeCredit = Math.Max(Audience - 30, 0);
+            // add extra credit for every ten comedy attendees
+            if ("comedy" == Play.Type) volumeCredit += (int)Math.Floor((decimal)Audience / 5);
+            return volumeCredit;
+        }
     }
 }
--- TheatricalPlayers/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayers/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -16,33 +16,12 @@ namespace TheatricalPlayersRefactoringKata
             foreach(var perf in invoice.Performances) 
             {
                 var play = perf.Play;
-                var thisAmount = 0;
-                switch (play.Type) 
-                {
-                    case "tragedy":
-                        thisAmount = 40000;
-                        if (perf.Audience > 30) {
-                            thisAmount += 1000 * (perf.Audience - 30);
-                        }
-                        break;
-                    case "comedy":
-                        thisAmount = 30000;
-                        if (perf.Audience > 20) {
-                            thisAmount += 10000 + 500 * (perf.Audience - 20);
-                        }
-                        thisAmount += 300 * perf.Audience;
-                        break;
-                    default:
-                        throw new Exception("unknown type: " + play.Type);
-                }
-                // add volume credits
-                volumeCredits += Math.Max(perf.Audience - 30, 0);
-                // add extra credit for every ten comedy attendees
-                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+                var thisAmount = perf.GetTotalAmount();
+                volumeCredits += perf.GetVolumeCredit();                
 
                 // print line for this order
                 result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
-                totalAmount += thisAmount;
+                totalAmount += perf.GetTotalAmount();
             }
             result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += String.Format("You earned {0} credits\n", volumeCredits);
